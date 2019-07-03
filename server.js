1e1c80eba5051ec0df961bc1bc23b2724414c623
elaintran <elain.ttran@gmail.com>
2019-07-03 13:58:01 -0500

    changed routing to reservations

--- server.js
+++ server.js
@@ -18,6 +18,30 @@ var waitlist = [];
 //     waitlist.push();
 // }
 
+// $("#submit").on("click", function(event) {
+//     event.preventDefault();
+//     var newReservation = {
+//       name: $("#name").val().trim(),
+//       phoneNumber: $("#phone-number").val().trim(),
+//       email: $("#email").val().trim(),
+//       uniqueID: $("#unique-id").val().trim()
+//     };
+
+//     if (reservations.length <= 5) {
+//         $.post("/api/tables", newReservation)
+//         .then(function(data) {
+//             console.log("add.html", data);
+//             alert("Adding character...");
+//         });
+//     } else {
+//         $.post("/api/waitlist", newReservation)
+//         .then(function(data) {
+//             console.log("add.html", data);
+//             alert("Adding character...");
+//         });
+//     }
+// });
+
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -28,8 +52,20 @@ app.get("/tables", function(req, res) {
 });  
 
 app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
+    res.sendFile(path.join(__dirname, "reservations.html"));
 });  
+
+app.post("/api/tables", function(req, res) {
+    var newTable = req.body;
+    reservations.push(newTable);
+    res.json(newTable);
+})
+
+app.post("/api/waitlist", function(req, res) {
+    var newTable = req.body;
+    waitlist.push(newTable);
+    res.json(newTable);
+})
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
